5b38554140ed1793670dbd75a72cd58a44e73cad
lukadubravcic <lukadubravcic@yahoo.com>
2015-11-03 17:43:19 +0100

    new

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+
+try {
+
+	$loader = new Loader();
+	$loader->registerDirs(array(
+		'/app/controllers',
+		'/app/models'
+	))->register();
+
+	$di = new FactoryDefault();
+
+	$di->set('view', function(){
+		$view = new View();
+		$view->setViewsDir('/app/views/');
+		return $view;
+	});
+
+	$di->set('url', function(){
+		$url = new UrlProvider();
+		$url->setBaseUri('/');
+		return $uri;
+	});
+
+	$application = new Application($di);
+
+	echo $application->handle()->getContent();
+
+} catch (\Exception $e) {
+	echo "PhalconException: ", $e->getMessage();
+}
